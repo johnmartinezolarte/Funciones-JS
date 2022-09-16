8b769844b8ecc13fce5cee0b07ff27bec316d151
John <johnmartinezolarte@gmail.com>
2022-09-16 16:47:48 -0500

    end of the lesson

--- main.js
+++ main.js
@@ -31,7 +31,24 @@ const sumar = function(a,b){
 }
 sumar(2,3); */
 /*Funcion que capture un numero y muestre la tabla de multiplicar de ese numero*/
-let number=+prompt("Give me a number");
+/* let number=+prompt("Give me a number");
 for(let i=1;i<10;i++){
     document.write(`${number}x${i}=${number*i}<br>`);
+} */
+/* scope global y local */
+function sumar(a,b){
+    let result=a+b;
+    /* console.log(result); */
+    return result;
 }
+function restar(a,b){
+    let result=a-b;
+    /* console.log(result); */
+    return result;
+}
+/* sumar(2,2);
+restar(3,2); */
+console.log(sumar(2,2));
+console.log(restar(3,2));
+let suma=sumar(2,2);
+let resta=restar(3,2);
