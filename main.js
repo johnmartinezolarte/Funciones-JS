1f0e6d425b3f51cd967238594e0b4ece7ccf3cf3
John <johnmartinezolarte@gmail.com>
2022-09-16 15:12:06 -0500

    Types of function

---
+++ main.js
@@ -0,0 +1,33 @@
+/*Declarada*/
+/* const a=1;
+const b=2;
+function sumar(){
+    let suma=a+b;
+    console.log(sumar);
+} */
+/* function sumar(a,b){
+    let suma=a+b;
+    console.log(suma);
+}
+sumar(2,1); */
+
+/*Expresada*/
+/* const sumar = function(a,b){
+    let suma=a+b;
+    console.log(suma);
+}
+sumar(2,1); */
+/* sumar(2,1);
+const sumar = function(a,b){
+    let suma=a+b;
+    console.log(suma);
+} */
+/* sumar(2,1); */
+/* arrow function */
+/* const sumar = (a,b) =>{
+    let suma=a+b;
+    console.log(suma);
+}
+sumar(2,3); */
+/*Funcion que capture un numero y muestre la tabla de multiplicar de ese numero*/
+let number=prompt("Give me a number");
